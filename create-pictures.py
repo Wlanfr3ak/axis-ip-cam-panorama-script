19ba4240ea287aea5ee62b78b1156b0015f9803e
Fabian Horst <6292882+Wlanfr3ak@users.noreply.github.com>
2021-01-08 15:32:22 +0100

    Create create-pictures.py
    Currently I am still waiting for a release permission to use the code

---
+++ create-pictures.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#based on https://nanuna.gi.alaska.edu/wiki/index.php/Camera_Operations_Using_Scripts
+
+
+!!!!!!!!!!
+Currently I am still waiting for a release permission to use the code
+!!!!!!!!!!
